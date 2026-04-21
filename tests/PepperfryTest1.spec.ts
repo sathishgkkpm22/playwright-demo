5b5e887a97887c198578c891c2a698c18e70fded
sathishgkkpm22 <163517266+sathishgkkpm22@users.noreply.github.com>
2026-04-21 12:17:38 +0530

    Delete tests directory
    delete1

--- tests/PepperfryTest1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.pepperfry.com/');
-  await page.locator('.close-modal').click();
-});
